d5ad43ba0e68b726de99418bfac33f3b304ea222
tokyoinfire <49620904+tokyoinfire@users.noreply.github.com>
2022-08-28 18:14:17 +0300

    firct commit

---
+++ 2.1.2/src/main/java/Cat.java
@@ -0,0 +1,59 @@
+public class Cat {
+
+    private String name;
+    private int age;
+    private int weight;
+
+    public Cat(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+
+        if (age != cat.age) return false;
+        if (weight != cat.weight) return false;
+        return name.equals(cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        result = 31 * result + weight;
+        return result;
+    }
+}
